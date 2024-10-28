03498074b7dba9a7184e0a9d0109d781ab259847
Dead insaan <119771032+Deadbrokenboy@users.noreply.github.com>
2024-10-28 09:37:25 +0530

    Update info.py

--- info.py
+++ info.py
@@ -43,9 +43,9 @@ SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'https://t.me/Noob_Marcus_Support') #
 #---------------------------------------------------------------
 IS_VERIFY = is_enabled('IS_VERIFY', True)
 #---------------------------------------------------------------
-TUTORIAL = environ.get("TUTORIAL", "https://t.me/how_to_download_movi/5")
+TUTORIAL = environ.get("TUTORIAL", "https://t.me/how_to_download_movi/16")
 TUTORIAL_2 = environ.get("TUTORIAL_2", "https://t.me/how_to_download_movi/5")
-TUTORIAL_3 = environ.get("TUTORIAL_3", "https://t.me/how_to_download_movi/16")
+TUTORIAL_3 = environ.get("TUTORIAL_3", "https://t.me/how_to_download_movi/5")
 VERIFY_IMG = environ.get("VERIFY_IMG", "https://graph.org/file/1669ab9af68eaa62c3ca4.jpg")
 SHORTENER_API = environ.get("SHORTENER_API", "d122f174a129c5e2b458fa40e78f3bcb1b647ca9")
 SHORTENER_WEBSITE = environ.get("SHORTENER_WEBSITE", 'modijiurl.com')
